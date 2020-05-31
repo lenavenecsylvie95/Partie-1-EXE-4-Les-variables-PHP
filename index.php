cb959f223b98f60bedf127b06d13c63079ef89b0
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-31 10:14:37 +0200

    OK

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>Partie 1 EXE 4 PHP</title>
+</head>
+<body>
+  <?php //balise d'ouverture - indique qu'on va écrire en php //A mettre dans le body d'un fichier HTML mais appelé PHP et non pas HTML
+  $name = 'sylvie'; ?>
+  <p> Bonjour <?php echo $name; ?>Comment vas-tu ? </p>
+</body>
+</html>
